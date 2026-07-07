5bf8a33f3a8ad243d45ac979d8e5e7793fed879d
md-Rafiqulislam <greenmate919@gmail.com>
2026-07-08 02:22:23 +0600

    modify products route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,43 +1,27 @@
-import { dbConnect } from "@/lib/db-connect-hanlder";
+
 import { productModel } from "@/models/product.model";
-import { NextRequest, NextResponse } from "next/server";
 import { sendResponse } from "../../../utils/send-response-hanlder";
 import { catchAsync } from "@/utils/catch-async-handler";
 import { productValidations } from "@/validations/product.validation";
 
 
 // create product
-const POST = async (req: NextRequest) => {
-    try {
-        await dbConnect();
+const POST = catchAsync(async (req) => {
+    const body = await req.json();
+    const validatedData = await productValidations.createProduct.parseAsync(body);
 
-        const body = await req.json();
-        const validatedData = await productValidations.createProduct.parseAsync(body);
+    const product = await productModel.create(validatedData);
 
-        const product = await productModel.create(validatedData);
-
-        sendResponse({
-            status: 201,
-            message: "Product created successfully.",
-            data: product,
-        });
-    } catch (error: any) {
-        return NextResponse.json(
-            {
-                status: 500,
-                success: false,
-                error: "Internal Server Error",
-                message: error.message || "An unexpected error occurred.",
-            },
-            { status: 500 }
-        );
-    }
-};
+    return sendResponse({
+        status: 201,
+        message: "Product created successfully.",
+        data: product,
+    });
+});
 
 
 // get products
-const GET = catchAsync(async (req: NextRequest) => {
-    await dbConnect();
+const GET = catchAsync(async (_req) => {
     const products = await productModel.find();
     return sendResponse({
         status: 200,
